57bb031ed975fb37ec455f4127ace2174bf2e44f
tonywujuncheng <1017226365@qq.com>
2020-04-06 19:16:41 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+Public class HelloWorld{
+	Public static void main(String[] args){
+		System.out.println("1");
+		System.out.println("Hello world");
+		System.out.println("2");
+		System.out.println("Hello world");
+
+	}
+}
